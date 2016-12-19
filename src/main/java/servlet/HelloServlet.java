637bb51b263b67d3d09cdddce6bafe143a62dff8
jimic0831 <jimic0831@sina.com>
2016-12-19 19:56:02 +0800

    ups

--- src/main/java/servlet/HelloServlet.java
+++ src/main/java/servlet/HelloServlet.java
@@ -1,9 +1,8 @@
-	package servlet;
+package servlet;
 
 import java.io.IOException;
-
+import java.io.PrintWriter;
 import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
